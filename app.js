d0cdb4057f0bae0a1cd72ac047d4c8b0979cb17e
Aleksandar <aleksandar.m.trifunovic@gmail.com>
2021-02-03 20:55:35 +0100

    lab done

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beer.hbs', {beersFromApi})
+    })
+    .catch(error => console.log(error));
+  
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      res.render('random-beers.hbs', { responseFromAPI });
+    })
+    .catch(error => console.log(error));
+  
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
